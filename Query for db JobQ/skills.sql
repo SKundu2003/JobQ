0a661289a58aa6df6ab672e593d3ba5e8e0899a4
Souvik Kundu <kundu9831562286@gmail.com>
2024-03-06 23:23:23 +0530

    create skill entity sql query

---
+++ Query for db JobQ/skills.sql
@@ -0,0 +1,7 @@
+use jobq;
+
+CREATE TABLE `skills` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `skill_name` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=latin1;
